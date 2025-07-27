edd356377b4cf7fc63a5169a1e8d07b9c0362f96
cBWtPDzay46dgJV5GUL7V3juC980HOikwTcBsL <kbaayar@gmail.com>
2025-07-27 20:05:44 +0300

    Update delwoof.bat

--- delwoof.bat
+++ delwoof.bat
@@ -1,28 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 
-
-
-
 mountvol X: /S
-
-
 move X:\EFI\Microsoft\Boot\boot.efi X:\EFI\Microsoft\Boot\bootmgfw.efi
-
-
 del X:\EFI\Boot\bootx64.efi
-
-
-del X:\EFI\Boot\sein.efi
-
-
+del X:\EFI\Boot\mp.efi
 move X:\EFI\Boot\bootx64.efi.backup X:\EFI\Boot\bootx64.efi
 
-
 mountvol X: /D
-
-
-
-
-
 exit
